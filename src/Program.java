5bfe3ee0ec715d335870984bb44d7bd3cbeb5eba
Mümin <mozdamar90@hotmail.com>
2022-10-11 20:56:34 +0300

    Yeni proje

---
+++ src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Yeni projeden herkese selamlar!");
+        System.out.println("-------------------------------");
+    }
+}
